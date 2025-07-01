3bf6899295815e7ec259c940caa90a62a72d81b0
Pedro Henrique Da Cruz <pedro07102006@gmail.com>
2025-06-30 22:54:06 -0300

    aula 11

---
+++ exercicios/mod d/aula11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log(`a velocidade do seu carro é de ${vel}`)
+if (vel > 60){ // condição simples
+    console.log(`Você foi multado`)
+}
+console.log(`Dirija sempre usando cinto de segunrança`)

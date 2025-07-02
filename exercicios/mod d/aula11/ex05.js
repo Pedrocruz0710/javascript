feade811edb00172b970cd5cc3b72903b55d748e
Pedro Henrique Da Cruz <pedro07102006@gmail.com>
2025-07-01 21:46:04 -0300

    finalização exercicio 01/07

---
+++ exercicios/mod d/aula11/ex05.js
@@ -0,0 +1,37 @@
+var agora = new Date()
+var diasm = agora.getDay
+
+/*
+0 == Domingo
+1 == segunda
+2 == terca 
+3 == quarta
+4 == quinta
+5 == sexta
+6 == sabado 
+7 == domingo
+*/
+
+switch(diasm){
+    case 0:
+        console.log('Domingo')
+    break
+    case 1:
+        console.log=('Segunda')
+        break
+    case 2:
+        console.log('Terça')  
+        break  
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    }

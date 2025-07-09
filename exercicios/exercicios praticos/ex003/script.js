7839ad807c963a617e87884c4e14b36f3bdfaba7
Pedro Henrique Da Cruz <pedro07102006@gmail.com>
2025-07-08 22:04:44 -0300

    dia 08/07

--- exercicios/exercicios praticos/ex003/script.js
+++ exercicios/exercicios praticos/ex003/script.js
@@ -1,26 +1,27 @@
 function contar(){
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById ('passo')
-    var valorinicio = Number(inicio.value)
+    var inicio = document.getElementById('inicio') // registra o dado vindo do input#inicio
+    var fim = document.getElementById('fim') // registra o dado vindo do input#fim
+    var passo = document.getElementById ('passo')//Registra o dado vindo do input#passo
+    var valorinicio = Number(inicio.value)// Estes tres asseguir transformam o dado em uma variavel tipo number
     var valorfim = Number(fim.value)
     var valorpasso = Number(passo.value)
-    var contagem = document.getElementById('contagem')
-    var c = valorinicio
-    var contando = document.getElementById ('contando')
+    var contagem = document.getElementById('contagem')// variavel atribuida a div#contagem
+    var c = valorinicio // varivel contadora, que é atribuida com o valor do inicio
     
-    contando.innerHTML ='Contando'
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    var contando = document.getElementById ('contando') // variavel atribuida ao paragrafo com id contando
+    
+    contando.innerHTML ='Contando'//muda a frase de preparando contagem, para, contando
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){ // quando iniciado sem dado qualquer campo, ele manda um alerta
         window.alert ('Verifique os dados')
         contando.innerHTML = 'Contagem invalida'
-    } else   if(valorpasso == 0 ){
+    } else   if(valorpasso == 0 ){ // se o valor de passo for == 0 ele manda um alerta
         contando.innerHTML = 'Contagem invalida'
         window.alert ('Verifique os dados')
     } else {do {
-        contagem.innerHTML += ` &#x1F449 ${c} `
-        c += valorpasso
-    }   while ( c <= valorfim)
-    contagem.innerHTML += '&#x1F6A9'}
+        contagem.innerHTML += ` &#x1F449 ${c} ` // mostra a contagem
+        c += valorpasso // a varaivel contadora recebe ela + o valor de passo
+    }   while ( c <= valorfim) // para quando chega no valor de fim
+    contagem.innerHTML += '&#x1F6A9'}// emoji de bandeira
       
     
     
---
+++ exercicios/exercicios praticos/ex004/script.js
@@ -0,0 +1,14 @@
+
+function carregar(){ // função que atribui o codigo ao elemento onclick
+    var numero = document.querySelector('input#numero') // regista o dado do input
+    var valnumero = Number(numero.value) // transforma o dado para numero, nunca esqueca do '.value', pois este dado ainda nao tem valor declarado
+    var res = document.getElementById('res')//variavel que esta atribuida a select com id res
+    var tabu = '' // valor que sera a tabuada
+
+        for (var c = 1; c <=10; c++){// aqui eu usei a estrutura de repetição 'for' pois ja declaro a variavel contadora direto nela
+            tabu = valnumero * c // multiplicação da tabuada
+            let item = document.createElement('option')// vai criar uma variavel com e para ela atribuir um elemento que sera criado, que sera o option
+            item.text = `${valnumero} x ${c} = ${tabu}`//o texto que ira aparecer na variavel item sera este
+            res.appendChild(item)//faz aparecer a variavel item  de filho do do select
+        }
+}
